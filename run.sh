6678a3e9f4f345279c72fd54ce234246b3b22032
cbuffin83 <cbuffin83@gmail.com>
2023-12-05 12:04:00 +0000

    amedned files

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo "I am running Successfully" > 1.txt
 echo "Yippee!" > 2.txt
-echo "ive only gone and added a webhook!" > 3,txt
+echo "ive only gone and added a webhook!" > 3.txt
 
 zip archive.zip *.txt 
